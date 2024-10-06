dc830e6473fa4295f8ee95fe81c815dd9f770e9e
Andrey Kareev <andrey.andreych@bk.ru>
2024-10-06 19:02:04 +0300

    Added gateway-service

---
+++ gateway-service/src/main/java/com/andrysheq/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,30 @@
+package com.andrysheq.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableHystrix
+@RequiredArgsConstructor
+public class GatewayConfig {
+
+    private final AuthenticationFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("book-author-service", r -> r.path("/book-author/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://book-author-service"))
+
+                .route("auth-service", r -> r.path("/auth/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://auth-service"))
+                .build();
+    }
+
+}

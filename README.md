679c3d5478f2ffabb7b3f80b77ac65c6c138ee4e
kareevaa <kareev@i-teco.ru>
2025-12-26 20:25:06 +0300

    nginx и kafka

--- README.md
+++ README.md
@@ -15,6 +15,11 @@ docker-compose up book-author-service -d
 docker compose build --no-cache user-moderation-service
 docker-compose up user-moderation-service -d
 
+# Build без кэша и запуск email-notification-service
+docker compose build --no-cache email-notification-service
+docker-compose up email-notification-service -d
+docker compose logs -f email-notification-service
+
 # Просмотр логов в реальном времени
 docker logs -f user-moderation-service
 
--- email-notification-service/src/main/java/moderation/notification/emailnotificationservice/service/EmailNotificationService.java
+++ email-notification-service/src/main/java/moderation/notification/emailnotificationservice/service/EmailNotificationService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import moderation.notification.emailnotificationservice.model.contract.UserStatusChangedEvent;
 import moderation.notification.emailnotificationservice.model.enums.UserStatusEnum;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -15,18 +16,19 @@ public class EmailNotificationService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendStatusChangedEmail(UserStatusChangedEvent event) {
         String subject = buildSubject(event);
         String body = buildBody(event);
 
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
         message.setTo(event.email());
         message.setSubject(subject);
         message.setText(body);
-
         mailSender.send(message);
-
-        log.info("Email о смене статуса отправлен на {}", event.email());
     }
 
     private String buildSubject(UserStatusChangedEvent event) {
